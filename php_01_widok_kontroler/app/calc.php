d8abd927989ebcb89813202ecd343f31c5b1f301
mariusz-bbrzck <116429585+mariusz-bbrzck@users.noreply.github.com>
2022-10-22 13:49:37 +0200

    Add files via upload

---
+++ php_01_widok_kontroler/app/calc.php
@@ -0,0 +1,65 @@
+<?php
+// KONTROLER strony kalkulatora
+require_once dirname(__FILE__).'/../config.php';
+
+// W kontrolerze niczego nie wysyła się do klienta.
+// Wysłaniem odpowiedzi zajmie się odpowiedni widok.
+// Parametry do widoku przekazujemy przez zmienne.
+
+// 1. pobranie parametrów
+
+$x = $_REQUEST ['x'];
+$y = $_REQUEST ['y'];
+$op = $_REQUEST ['op'];
+
+// 2. walidacja parametrów z przygotowaniem zmiennych dla widoku
+
+// sprawdzenie, czy parametry zostały przekazane
+if ( ! (isset($x)  && isset($y) && isset($op))) {
+	//sytuacja wystąpi kiedy np. kontroler zostanie wywołany bezpośrednio - nie z formularza
+	$messages [] = 'Błędne wywołanie aplikacji. Brak jednego z parametrów.';
+}
+
+// sprawdzenie, czy potrzebne wartości zostały przekazane
+if ( $x == "") {
+	$messages [] = 'Nie podano liczby 1';
+}
+if ( $y == "") {
+	$messages [] = 'Nie podano liczby 2';
+}
+
+//nie ma sensu walidować dalej gdy brak parametrów
+if (empty( $messages )) {
+	
+	// sprawdzenie, czy $x i $y są liczbami całkowitymi dodatnimi
+	if (! is_numeric( $x )|| $x <= 0) {
+		$messages [] = 'Kwota nie jest liczbą dodatnią całkowitą';
+	}
+	
+	if (! is_numeric( $y )|| $y <= 0) {
+		$messages [] = 'Lata nie są liczbą dodatnią całkowitą';
+	}	
+
+	if (! is_numeric( $op ) || $op <= 0) {
+	$messages [] = 'Oprocentowanie nie jest dodatnią liczbą całkowitą';
+	}
+}
+
+// 3. wykonaj zadanie jeśli wszystko w porządku
+
+if (empty ( $messages )) { // gdy brak błędów
+	
+	//konwersja parametrów na int
+	$x = intval($x);
+	$y = intval($y);
+	$op = floatval($op);
+	
+	//wykonanie operacji
+	$rata = $x/($y*12);
+	$result = round($rata*($op/100)+$rata,2);
+}
+
+// 4. Wywołanie widoku z przekazaniem zmiennych
+// - zainicjowane zmienne ($messages,$x,$y,$operation,$result)
+//   będą dostępne w dołączonym skrypcie
+include 'calc_view.php';
